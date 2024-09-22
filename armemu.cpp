ede1bd2165e0c3676aacc9db64c949fff6a36f69
Kevin Nnaji <2729400n@student.gla.ac.uk>
2024-09-22 05:08:30 +0100

    Updated logic for instruction multiplexor
    Added a few fixes

--- armemu.cpp
+++ armemu.cpp
@@ -1,106 +1,132 @@
 #include "armemu.hpp"
 #include <iostream>
 using namespace std;
-using namespace ARMISA;
+namespace ARMISA
+{
+    
 
+
+
+FlagReg getFlags(long long result ){
+   FlagReg reg;
+   reg.zf= result==0;
+   reg.sf= ((result>>31)&1)&&((result>>32)!=0);
+   reg.of=(((result>>31)&1)&&((result>>32)==0));
+   reg.cf= (result>>32)!=0;
+   return reg;
+}
 
 
 int rotateOP(int op,int rot){
-    return (op>>rot)|(op<<(32-rot))
+    return (op>>rot)|(op<<(32-rot));
 }
 
 int shiftOP(int op,int shift){
-    return op>>
+    switch (shift)
+    {
+    default:
+        break;
+    }
+    return op>>shift;
 }
 
 void UD2(ARMMachine* inst, int instruction){
     printf("Instuction Not Implemented #UD2(%#x)\n",instruction);
 }
 
-void movw(ARMMachine* inst, int instruction){
-    int register_=(instruction>>12)&0xf,value = (instruction&0xff)|(((instruction>>8)&0xf)|((instruction>>12)&0xf0));
-    inst->registers.regs[register_] = (inst->registers.regs[register_]&0xffff0000)|(value&0xffff);
-}
-void movt(ARMMachine* inst, int instruction){
-    int register_=(instruction>>12)&0xf,value = (instruction&0xff)|(((instruction>>8)&0xf)|((instruction>>12)&0xf0));
-    inst->registers.regs[register_] = (inst->registers.regs[register_]&0xffff)|(value<<16);
+int mov(int op1,int op2){
+    return op2;
 }
 
-void and_(ARMMachine* inst, int instruction){
-    DPINST* instruct= (DPINST*)&instruction;
-    int op2=instruct->I==1?rotateOP(inst->registers.regs[instruct->op2&0xff],(instruct->op2&0xf00)>>8),shiftOP(inst->registers.regs[instruct->op2&0xf],(instruct->op2&0xff0)>>4);
-    int result= instruct->Rn&op2;
+int bic(int op1,int op2){
+    return op1&(~op2);
 }
 
-void orr(ARMMachine* inst, int instruction){
-    DPINST* instruct=(DPINST*) &instruction;
-    int op2=instruct->I==1?rotateOP(inst->registers.regs[instruct->op2&0xff],(instruct->op2&0xf00)>>8),shiftOP(inst->registers.regs[instruct->op2&0xf],(instruct->op2&0xff0)>>4);
-    int result= instruct->Rn|op2;
+long long and_(int op1,int op2){
+    return op1&op2;
 }
 
-void eor(ARMMachine* inst, int instruction){
-    DPINST* instruct=(DPINST*)&instruction;
-    int op2=instruct->I==1?rotateOP(inst->registers.regs[instruct->op2&0xff],(instruct->op2&0xf00)>>8),shiftOP(inst->registers.regs[instruct->op2&0xf],(instruct->op2&0xff0)>>4);
-    int result= instruct->Rn^op2;
+long long orr(int op1,int op2){
+    return op1|op2;
 }
 
-void add( ARMMachine* inst, int instruction){
-    DPINST* instruct=(DPINST*) &instruction;
-    int op2=instruct->I==1?rotateOP(inst->registers.regs[instruct->op2&0xff],(instruct->op2&0xf00)>>8),shiftOP(inst->registers.regs[instruct->op2&0xf],(instruct->op2&0xff0)>>4);
-    int result= instruct->Rn+op2;
+long long eor(int op1,int op2){
+    return op1^op2;
 }
 
-void sub(ARMMachine* inst, int instruction){
-    DPINST* instruct=(DPINST*) &instruction;
-    int op2=instruct->I==1?rotateOP(inst->registers.regs[instruct->op2&0xff],(instruct->op2&0xf00)>>8),shiftOP(inst->registers.regs[instruct->op2&0xf],(instruct->op2&0xff0)>>4);
-    int result= instruct->Rn-op2;
+long long add(int op1,int op2){
+    return op1+op2;
 }
 
-void adc(ARMMachine* inst, int instruction){
-    DPINST* instruct=(DPINST*) &instruction;
-    int op2=instruct->I==1?rotateOP(inst->registers.regs[instruct->op2&0xff],(instruct->op2&0xf00)>>8),shiftOP(inst->registers.regs[instruct->op2&0xf],(instruct->op2&0xff0)>>4);
+long long sub(long long op1,int op2){
+    return op1-op2;
 }
 
-void mul(ARMMachine* inst, int instruction){
-    DPINST* instruct=(DPINST*) &instruction;
-    int op2=instruct->I==1?rotateOP(inst->registers.regs[instruct->op2&0xff],(instruct->op2&0xf00)>>8),shiftOP(inst->registers.regs[instruct->op2&0xf],(instruct->op2&0xff0)>>4);
+long long mul(ARMMachine* inst, int op1,int op2){
+    return op1*op2;
+
 }
 
 void MathHUB(ARMMachine* inst, int instruction){
-    DPINST* instruct =(DPINST*)&instruction;
+    DPINST* instruct=(DPINST*) &instruction;
+    int *dest = &(inst->registers.regs[instruct->rd]),op1=inst->registers.regs[instruct->Rn],
+    op2=instruct->I==1?rotateOP(inst->registers.regs[instruct->op2&0xff],(instruct->op2&0xf00)>>8):shiftOP(inst->registers.regs[instruct->op2&0xf],(instruct->op2&0xff0)>>4);
+    long long result =0;
     if(instruct->OPCODE<CMN){
     switch(instruct->OPCODE &7){
         case AND:
-            and_(inst,instruct);
+            
+            result =and_(op1,op2);
             break;
+
         case EOR:
-            eor(inst,instruct);
+            result =eor(op1,op2);
             break;
-        case SUB:
-            sub(inst,instruct);
+
+        case SBC:
+        case RSC:
+            op2+=inst->registers.Flags.cf-1;
+        case RSB: 
+            if((instruct->OPCODE&1)!=0){
+                int temp=op1;
+                op1=op2;
+                op2=temp;
+            }            
+        case SUB:        
+            result =sub(op1,op2);
             break;
-        case ADD:
-            add(inst,instruct);
+
+        case ADC:  
+            op2+=inst->registers.Flags.cf;  
+        case ADD:                
+            result =add(op1,op2);
             break;
     }
+    if(instruct->S){
+        inst->setFlags(getFlags(result));
+    }
+    if(instruct->OPCODE&8!=0){
+        *dest=result;
+    }
+    return;
     }else{
     switch (instruct->OPCODE)
     {
 
     case CMN:
-        add(inst,instruct);
+        add(op1,op2);
         break;
     case ORR:
-        orr(inst,instruct);
+        orr(op1,op2);
         break;
     case MOV:
-        mov(inst,instruct);
+        mov(op1,op2);
         break;
     case BIC:
-        bic(inst,instruct);
+        bic(op1,op2);
         break;
     case MVN: 
-        mov(inst,instruct);
+        mov(op1,~op2);
         break;
     }
     }
@@ -113,15 +139,60 @@ void blockTransfer(ARMMachine* inst, int instruction){
 
 }
 
-class ARMEmulatorHTB:ARMMachine{
+class ARMEmulatorHTB:public ARMMachine{
     public:
     ARMEmulatorHTB(){
 
     }
+
+    void ResetCLR(){
+            memset(&registers,0,sizeof(registerFile_t));     
+        }
+    bool shouldExec(char condition){
+        switch (condition)
+        {
+        case EQ:
+            return this->registers.Flags.zf==1;                     
+        case NE:    
+            return this->registers.Flags.zf==0;
+        case CS:
+            return this->registers.Flags.cf==1;
+        case CC:    
+            return this->registers.Flags.cf==0;
+        case MI:
+            return this->registers.Flags.sf==1;
+        case PL:    
+            return this->registers.Flags.sf==0;
+        case VS:
+            return this->registers.Flags.of==1;
+        case VC:    
+            return this->registers.Flags.of==0; 
+        case HI:
+            return (this->registers.Flags.cf&!this->registers.Flags.zf)==1;
+        case LS:    
+            return !((this->registers.Flags.cf&!this->registers.Flags.zf)==1);
+        case GE:
+            return this->registers.Flags.sf==this->registers.Flags.of;
+        case LT:    
+            return !(this->registers.Flags.sf==this->registers.Flags.of);
+        case GT:
+            return (this->registers.Flags.zf==1)&&(this->registers.Flags.sf==this->registers.Flags.of);
+        case LE:    
+            return !((this->registers.Flags.zf==1)&&(this->registers.Flags.sf==this->registers.Flags.of));
+        default:
+            return true;
+        }
+    }
     void execute(const int n){
+        
         logicalBreakdown_t* logict = (logicalBreakdown_t*) &n;
+        if(!shouldExec(logict->condition)){
+            printf("NOT executing intruction #%#x\n",(this->registers.pc-1)*4);
+            return;
+        }
         int result=0;
         if((logict->logicalBranch&0x6)==6){
+            printf("COPROC intruction #%#x\n",(this->registers.pc-1)*4);
             return;
         }else{
             switch (logict->logicalBranch)
@@ -136,20 +207,54 @@ class ARMEmulatorHTB:ARMMachine{
                 break;
             }
             if((logict->logicalBranch&2)!=0){
-                
+                printf("BRANCh intruction #%#x\n",(this->registers.pc-1)*4);
             }else if(logict->branch==1){
-
+                 printf("BRANCh intruction #%#x\n",(this->registers.pc-1)*4);
             }else if((((MultInstruction*) logict)->opCode==9)&&((logict->branch+logict->logicalBranch)==0)){
-                mul(this,n);
+                MultInstruction* z=((MultInstruction*) logict);
+                printf("MUL intruction #%#x\n",(this->registers.pc-1)*4);
             }else{
                 MathHUB(this,n);
+                //printf("Executing dsp intruction #%#x\n",(this->registers.pc-1)*4);
+                //fprintf(stderr,"oppcode %d\n",((MultInstruction*) logict)->opCode);
             }
+            
         }
 
     }
-};
+    void load(int* code,int size){
+            this->ResetCLR();
+            this->codeSize=size;
+            this->base=code;
 
+    } 
+    void load(FILE* codeFile){
+            this->ResetCLR();
+            struct stat filedata;
+            fstat(fileno(codeFile),&filedata);
+            this->codeSize=filedata.st_size;
+            this->base=(int*)malloc(this->codeSize);
+            fread_s(this->base,this->codeSize,1,this->codeSize,codeFile);            
+    } 
+    void exec(){
+        printf("Executing ARM binary\n");
+            while (this->registers.Flags.hlt!=1 && (this->registers.pc<(codeSize/4))){
+                
+                this->execute(this->base[this->registers.pc++]);
+            }    
+    
+    }
+    
+};
+} // namespace ARMISA
+using namespace ARMISA;
 int main(){
-    ARMEmulatorHTB();
+    ARMEmulatorHTB* HTB=new ARMEmulatorHTB();
+    FILE* binary = fopen("ARM.FLAT.BIN","rb");
+
+    HTB->load(binary);
+    fclose(binary);
+    printf("Loaded ARM binary\n");
+    HTB->exec();
     return 0;
 }
--- armemu.hpp
+++ armemu.hpp
@@ -8,10 +8,9 @@
 #include <memory>
 #include <cstring>
 #include "Memulator.hpp"
-namespace ARMISA{
+namespace ARMISA {
 struct ARMMachine;
-typedef void (*arminst)(ARMMachine* ,int op);
-arminst ops[256];
+
 typedef struct logicalBreakdown{
     int unused:24,branch:1,logicalBranch:3,condition:4;
 }logicalBreakdown_t;
@@ -27,11 +26,11 @@ enum Conditions{
     VC=0b0111, // Overflow clear
     HI=0b1000, // US higher
     LS=0b1001, // US LOWER or SAME
-    GE=0b1010,
+    GE=0b1010, // Greater than or Equal
     LT=0b1011,
     GT=0b1100,
     LE=0b1101,
-    AL=0b1110
+    AL=0b1110 // no checks
 };
 enum SHIFTTYPE{
     shl,
@@ -40,28 +39,28 @@ enum SHIFTTYPE{
     ror
 };
 struct DPINST{
-    int op2:12,rd:4,Rn:4,S:1,OPCODE:4,I:1,unused:2,cond:4;   
+    unsigned int op2:12,rd:4,Rn:4,S:1,OPCODE:4,I:1,unused:2,cond:4;   
 };
 struct MultInstruction{
-    int rm:4,opCode:4,Rn:4,RdLo:4,RdHi:4,S:1,A:1,U:1;
-}
+    unsigned int rm:4,opCode:4,Rn:4,RdLo:4,RdHi:4,S:1,A:1,U:1;
+};
 struct OP2Reg{
-    int Rm:4,shift:8,unused:20;
+    unsigned int Rm:4,shift:8,unused:20;
 };
 struct OP2Imm{
-    int Rm:8,shift:4,unused:20;
+    unsigned int Rm:8,shift:4,unused:20;
 };
 struct BX{
-    int rn:4,unused:24,cond:4;   
+    unsigned int rn:4,unused:24,cond:4;   
 };
 struct BL{
-    int offet:24,Link:1,unused:3,cond:4;   
+    unsigned int offet:24,Link:1,unused:3,cond:4;   
 };
 struct MLA{
-    int Rm:4,unused1:4,Rs:4,Rn:4,Rd:4,SS:1,A:1,unused:6,cond:4;
+    unsigned int Rm:4,unused1:4,Rs:4,Rn:4,Rd:4,SS:1,A:1,unused:6,cond:4;
 };
 struct MLAL{
-    int Rm:4,unused1:4,Rs:4,Rn:4,Rd:4,SS:1,A:1,U:1,unused:5,cond:4;
+    unsigned int Rm:4,unused1:4,Rs:4,Rn:4,Rd:4,SS:1,A:1,U:1,unused:5,cond:4;
 };
 
 
@@ -84,6 +83,10 @@ enum DATACSPR{
     MVN
 };
 
+struct FlagReg {
+    int zf:1,of:1,cf:1,af:1,ov:1,sf:1,hlt:1,unused:25;
+} ;
+
 typedef struct registerFile{
     union 
     {
@@ -91,10 +94,7 @@ typedef struct registerFile{
         struct 
         {
             int r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,sp,lr,pc;
-            struct 
-            {
-                int zf:1,of:1,cf:1,af:1,ov:1,sf:1,hlt:1;
-            } Flags;
+            FlagReg Flags;
             
 
         };
@@ -105,53 +105,36 @@ typedef struct registerFile{
 
 
 
-
+enum SHIFTER{
+    SHL,
+    SHR,
+    SAR,
+    ROR
+};
 
 
 struct ARMMachine:public SMALLMemULatorBase{
-    private:
+    protected:
         int codeSize=0;
         int *base=NULL;
     public:
         registerFile_t registers={0};
-        void ResetCLR(){
-            memset(&registers,0,sizeof(registerFile_t));     
-        }
-        arminst intructionTable[255];
+        void ResetCLR();
+        bool shouldExec(char condition);
+        void execute(const int n);
+        void load(int* code,int size);
+        void load(FILE* codeFile);
+        void exec();
+        void setFlags(FlagReg flags){
+        this->registers.Flags=flags;
+    }
+};
 
-        
-        ARMMachine();
-
-        /**
-         * ArmMachine contructor
-         * <param name="builder">
-         *  Does some of the hard and heavy lifting such as building a 
-         *  fake arm intrutin set and a nice reduction to cisc. 
-         *  This procedure should take a intance pointer to an ARMMachine
-         * </param> 
-         */
-        ARMMachine(int* code,void (*builder)(ARMMachine*) );
-
-        ~ARMMachine();
-
-    void load(int* code,int size){
-        this->ResetCLR();
-        this->base=code;
-        this->codeSize=size;
-    } 
-
-
-    virtual void execute(int)=0;
-
-    void execute(){
-
-        while (this->registers.Flags.hlt!=1 && (this->registers.pc<=codeSize)){
-            this->execute(this->base[this->registers.pc++]);
-        }    
+}
+/*class ARMEmulatorHTB:public ARMMachine{
+    public:
+    ARMEmulatorHTB(){
 
     }
-
-
 };
-}
-
+}*/
